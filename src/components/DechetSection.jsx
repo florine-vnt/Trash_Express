3186cd2d6a59d4fc16efc65d6c781d8b6c6d49a2
TheoDegorre <degorret@gmail.com>
2024-05-17 13:36:20 +0200

    feature dechetSection done

--- src/components/DechetSection.jsx
+++ src/components/DechetSection.jsx
@@ -2,7 +2,6 @@
 import { NavLink, useLoaderData } from "react-router-dom";
 import { useState } from "react";
 
-
 import DechetCard from "./DechetCard";
 import Map from "./Map";
 import "../styles/dechet-section.css";
@@ -16,32 +15,37 @@ function DechetSection() {
     setText(e.target.value.toLowerCase());
   }
 
-  const filteredData = (text !== "" ? (data.filter((trash) => trash.commune.toLowerCase().includes(text) )) : data);
+  const filteredData =
+    text !== ""
+      ? data.filter((trash) => trash.commune.toLowerCase().includes(text))
+      : data;
 
   return (
     <section className="dechet-section">
-      <div className="trash-part">
-        <h2>Trouvez la dechetterie de vos rêves</h2>
-        <input
-          type="texte"
-          placeholder="Trouvez la dechetterie de vos rêves"
-          alt="barre de recherche"
-          onChange={handleChange}
-          value={text}
-        />
+      <div className="background">
+        <div className="trash-part">
+          <h2>Trouvez le dépôt de vos rêves</h2>
+          <input
+            type="texte"
+            placeholder="  Cherchez votre commune"
+            alt="barre de recherche"
+            onChange={handleChange}
+            value={text}
+          />
 
-        <ul>
-          <li className="trash-card">
-            {filteredData.map((trash) => (
-              <NavLink className="trash-card" to={`/detail/${trash.id}`}>
-                <DechetCard key={trash.id} data={trash} />
-              </NavLink>
-            ))}
-          </li>
-        </ul>
+          <ul>
+            <li className="trash-card">
+              {filteredData.map((trash) => (
+                <NavLink className="trash-card" to={`/detail/${trash.id}`}>
+                  <DechetCard key={trash.id} data={trash} />
+                </NavLink>
+              ))}
+              </li>
+          </ul>
+        </div>
+
+        <Map className="trash-map" filteredData={filteredData} />
       </div>
-
-      <Map filteredData={filteredData}/>
     </section>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,26 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
+import { NavLink } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function Map({ filteredData }) {
+  console.log("ceci est filteredData", filteredData);
 
   return (
     <div className="map">
-      <MapContainer center={[46.78, 2.7]} zoom={6.5}>
+      <MapContainer center={[46.78, 2.7]} zoom={6.2}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
         />
 
-        {filteredData.map(marker => (
-            <Marker position ={marker.geocode}>
-
+        {filteredData.map((marker) => (
+          <NavLink className="trash-card" to={`/detail/${marker.id}`}>
+            <Marker position={marker.geocode}>
+              <Popup><b>{marker.commune}</b></Popup>
             </Marker>
+          </NavLink>
         ))}
       </MapContainer>
     </div>
