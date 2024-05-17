6dd50e34c53a4a1dcf823b39f3ce4c5d724833e1
Micka972 <dem972md@gmail.com>
2024-05-17 12:10:54 +0200

    disclaimer page in progress

--- src/pages/DisclaimerPage.jsx
+++ src/pages/DisclaimerPage.jsx
@@ -1,18 +1,19 @@
 import { image_planete } from "../data";
-import "../styles/disclaimer-page.css"
-import But from '../components/But';
+import "../styles/disclaimer-page.css";
+import But from "../components/But";
 import Law from "../components/Law";
 
 function DisclaimerPage() {
-  let randomImage = image_planete[Math.floor(Math.random() * image_planete.length)]
+  let randomImage =
+    image_planete[Math.floor(Math.random() * image_planete.length)];
 
   return (
     <section className="disclaimer-page">
       <img src={randomImage.image} alt="illustration planète personnage" />
-     <But />
-    <Law />
-    </section> 
-  )
+      <But />
+      <Law />
+    </section>
+  );
 }
 
 export default DisclaimerPage;
