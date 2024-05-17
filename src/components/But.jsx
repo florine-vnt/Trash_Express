baaf213a017114e51464950dc5ade955eace4d8e
Micka972 <dem972md@gmail.com>
2024-05-17 14:51:43 +0200

    modif but.jsx

--- src/components/But.jsx
+++ src/components/But.jsx
@@ -1,20 +1,39 @@
-import '../styles/But.css'
-import React from 'react'
+import "../styles/But.css";
+import React from "react";
 
 const But = () => {
   return (
     <>
-    <article className='articlebut'> 
-    <h2 className='but'>
-    <h2 className='important'>Important</h2>
-    <p>Ce site présente des contenus qui peuvent être choquants ou dérangeants pour certains individus en raison de leur nature réaliste et dépeignent les conséquences désastreuses de l'impact humain sur l'environnement. Notre intention n'est pas de choquer gratuitement, mais de susciter une réflexion profonde sur les actions et les choix de chacun en ce qui concerne la protection de notre planète.</p>
-    <p>Nous tenons à souligner que les images et les informations présentées ici sont basées sur des faits scientifiques et des réalités actuelles de notre environnement. En les exposant, nous visons à sensibiliser le public à l'urgence d'adopter des comportements écologiquement responsables et de prendre des mesures concrètes pour préserver notre habitat commun.</p>
-    <p>Nous croyons fermement que le recyclage et l'achat de produits reconditionnés sont des moyens efficaces de réduire notre empreinte écologique. En donnant une seconde vie aux matériaux et aux produits, nous pouvons prolonger leur utilité et réduire la quantité de déchets envoyés dans les décharges. De plus, privilégier les produits reconditionnés peut également contribuer à réduire la demande de nouvelles ressources, ce qui a un impact positif sur l'environnement.</p>
-    <p>En naviguant sur ce site, vous acceptez de prendre connaissance de ces contenus et de participer à la conversation sur la préservation de notre planète. Ensemble, nous pouvons faire la différence.</p>
-    </h2> 
-    </article>
-  </>
-  )
-}
+      <article className="articlebut">
+        <h2 className="but">
+          <h2 className="important">Important</h2>
+          <p>
+            Ce site présente des contenus qui peuvent être choquants ou
+            dérangeants pour certains individus en raison de leur nature
+            réaliste et dépeignent les conséquences désastreuses de l'impact
+            humain sur l'environnement. Notre intention n'est pas de choquer
+            gratuitement, mais de susciter une réflexion profonde sur les
+            actions et les choix de chacun en ce qui concerne la protection de
+            notre planète.
+            <br /> En les exposant, nous visons à sensibiliser le public à
+            l'urgence d'adopter des comportements écologiquement responsables et
+            de prendre des mesures concrètes pour préserver notre habitat
+            commun.
+          </p>
+          <p>
+            Nous croyons fermement que le recyclage et l'achat de produits
+            reconditionnés sont des moyens efficaces de réduire notre empreinte
+            écologique. En donnant une seconde vie aux matériaux et aux
+            produits, nous pouvons prolonger leur utilité et réduire la quantité
+            de déchets envoyés dans les décharges. De plus, privilégier les
+            produits reconditionnés peut également contribuer à réduire la
+            demande de nouvelles ressources, ce qui a un impact positif sur
+            l'environnement.
+          </p>
+        </h2>
+      </article>
+    </>
+  );
+};
 
-export default But
+export default But;
