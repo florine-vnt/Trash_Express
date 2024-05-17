ba21e8f64d3336590a48ebf4f2feddec71389f75
Florine <florinevinet@gmail.com>
2024-05-17 09:47:18 +0200

    random name & date added

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const data_names = [
+    {
+        name : "Alexis",
+        date : "il y a 2 semaines",
+    },
+    {
+        name : "Michaël C",
+        date : "il y a 1 mois",
+    },
+    {
+        name : "Martin",
+        date : "il y a 1 semaine",
+    },
+    {
+        name : "Mickaël D",
+        date : "il y a 2 mois",
+    },
+    {
+        name : "Antoine",
+        date : "il y a 6 semaines",
+    },
+    {
+        name : "Audren",
+        date : "il y a 4 mois",
+    },
+    {
+        name : "Stéphanie",
+        date : "il y a 4 semaines",
+    },
+    {
+        name : "Théo",
+        date : "il y a 3 mois",
+    },
+    {
+        name : "Florine",
+        date : "il y a 1 semaine",
+    },
+    {
+        name : "Lucas",
+        date : "il y a 1 mois",
+    },
+    {
+        name : "Steven",
+        date : "il y a 2 semaines",
+    },
+    {
+        name : "David",
+        date : "il y a 2 mois",
+    },
+    {
+        name : "Adeline",
+        date : "il y a 5 semaines",
+    },
+    {
+        name : "Julien",
+        date : "il y a 6 mois",
+    },
+    {
+        name : "Pascal",
+        date : "il y a 7 semaines",
+    },
+    {
+        name : "Hakim",
+        date : "il y a 1 mois",
+    },
+    {
+        name : "Nicolas",
+        date : "il y a 6 semaines",
+    },
+    {
+        name : "Lukasz",
+        date : "il y a 2 mois",
+    }, 
+]
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,14 +1,18 @@
 import { useLoaderData } from "react-router-dom";
 import "../styles/detail-page.css"
+import { data_names } from "../data";
 
 function DetailPage() {
   const data = useLoaderData();
+
+  let randomDatas = data_names[Math.floor(Math.random() * data_names.length)]
+  console.log(randomDatas)
 
   return (
     <figure className="detail-page">
       <h2>Zoom sur la décharge de {data.commune}</h2>
       <img src={data.image} alt="photo d'une décharge" />
-      <p>" {data.avis} "</p>
+      <p>"{data.avis}" {randomDatas.name}, {randomDatas.date}.</p>
     </figure>
   )
   
