5d3ea78b1b1574abe5b807ac045d1b38e1e63e95
Florine <florinevinet@gmail.com>
2024-05-17 11:09:58 +0200

    random planete works

--- src/pages/DisclaimerPage.jsx
+++ src/pages/DisclaimerPage.jsx
@@ -1,4 +1,14 @@
+import { image_planete } from "../data";
+import "../styles/disclaimer-page.css"
+
 function DisclaimerPage() {
-  return <></>;
+  let randomImage = image_planete[Math.floor(Math.random() * image_planete.length)]
+
+  return (
+    <section className="disclaimer-page">
+      <img src={randomImage.image} alt="illustration planète personnage" />
+
+    </section> 
+  )
 }
 export default DisclaimerPage;
